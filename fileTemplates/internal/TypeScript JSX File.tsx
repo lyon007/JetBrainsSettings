05f075215c6038de93a39fd5dcdb8d164561b67e
lyon <13077339896@163.com>
2019-08-15 16:56:59 +0800

    WS-2019.1.3 <jiangshunxin@jiangshunxindeMacBook-Pro.local Create baseRefactoring.xml, markdown.xml, JavaScript File Header .jsx, Vue Single File Component.vue, ide.general.xml, React Native  JS.js, project.default.xml, diff.xml, Default.xml, file.template.settings.xml, find.xml, ui.lnf.xml, filetypes.xml, watcherDefaultTasks.xml, colors.scheme.xml, _@user_Material Oceanic.icls, External Tools.xml, TypeScript JSX File.tsx, _@user_Darcula.icls, path.macros.xml, github.xml, JavaScript File.js, Default.xml, JavaScript.xml, px2remforwebstorm.xml, phonegap.xml, laf.xml, Mac OS X 10_5_ copy.xml, material_theme.xml, proxy.settings.xml, vcs.xml, editor.codeinsight.xml, keymap.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/TypeScript JSX File.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+
+interface PropsIF {
+
+}
+
+interface StateIF {
+
+}
+
+export default class ${NAME} extends React.Component<PropsIF, StateIF> {
+  constructor(props: PropsIF){
+    super(props)
+  }
+  
+  public render(){
+    return (
+        <div>
+        
+        </div>
+    )
+  }
+}
